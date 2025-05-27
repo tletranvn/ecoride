c47f349c0b360093a668651b37862b35ba257a2c
tletranvn <tletranvn@gmail.com>
2025-05-27 10:17:48 +0200

    OK US1/US2 - ajout barre de recherche

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class HomeController extends AbstractController
 {
@@ -22,6 +23,17 @@ final class HomeController extends AbstractController
         return $this->render('mentions-legales.html.twig');
     }
 
+    #[Route('/rechercher-trajet', name: 'app_rechercher_trajet', methods: ['GET'])]
+    public function rechercherTrajet(Request $request): Response
+    {
+        $villeDepart = $request->query->get('ville_depart');
+        $villeArrivee = $request->query->get('ville_arrivee');
 
+        // Pour l'instant, on affiche juste les valeurs saisies
+        return $this->render('recherche/resultats.html.twig', [
+            'villeDepart' => $villeDepart,
+            'villeArrivee' => $villeArrivee,
+        ]);
+    }
 
 }
