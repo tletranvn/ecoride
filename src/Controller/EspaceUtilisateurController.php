2e20eff1c4c2746e7ec15ad962a3103b1f214b2c
tletranvn <tletranvn@gmail.com>
2025-06-04 14:57:45 +0200

    Ok US8 avec ChoixProfil - enlever userType à l'inscription

--- src/Controller/EspaceUtilisateurController.php
+++ src/Controller/EspaceUtilisateurController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\ChoixProfilType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\VehiculeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 #[IsGranted('ROLE_USER')]
 class EspaceUtilisateurController extends AbstractController
@@ -22,4 +26,28 @@ class EspaceUtilisateurController extends AbstractController
             'vehicules' => $vehicules,
         ]);
     }
+
+    // Route pour choisir le profil de l'utilisateur US8
+    // Cette route permet à l'utilisateur de choisir son rôle (passager, chauffeur, passager + chauffeur)
+    // Le formulaire est géré par le type ChoixProfilType
+    // Après la soumission du formulaire, le rôle de l'utilisateur est mis à jour dans la base de données
+    #[Route('/espace-utilisateur/choix-profil', name: 'app_choix_profil')]
+    public function choixProfil(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ChoixProfilType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre rôle a été mis à jour.');
+            return $this->redirectToRoute('app_espace_utilisateur');
+        }
+
+        return $this->render('espace_utilisateur/choix-profil.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
--- src/Form/RegistrationForm.php
+++ src/Form/RegistrationForm.php
@@ -12,7 +12,7 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+//use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 
@@ -56,17 +56,17 @@ class RegistrationForm extends AbstractType
             ])
             
             //ajouter userType pour US8 passager, chauffeur, passager_chauffeur
-            ->add('userType', ChoiceType::class, [
-                'label' => 'Quel type d’utilisateur êtes-vous ?',
-                'choices' => [
-                    'Passager' => 'passager',
-                    'Chauffeur' => 'chauffeur',
-                    'Passager + Chauffeur' => 'passager_chauffeur',
-                ],
-                'expanded' => true, // affichage en boutons radio
-                'multiple' => false,
-                'required' => true,
-            ])
+            //->add('userType', ChoiceType::class, [
+            //    'label' => 'Quel type d’utilisateur êtes-vous ?',
+            //    'choices' => [
+            //        'Passager' => 'passager',
+            //        'Chauffeur' => 'chauffeur',
+            //        'Passager + Chauffeur' => 'passager_chauffeur',
+            //    ],
+            //    'expanded' => true, // affichage en boutons radio
+            //    'multiple' => false,
+            //    'required' => true,
+            //])
 
         ;
     }
